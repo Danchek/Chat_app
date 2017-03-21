801fd1e4e271546771696b57dbc4ad0b03bbfcd5
Danchek <denisdotsenko7@gmail.com>
2017-03-21 17:28:54 +0200

    Add websockets

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  root 'chat_rooms#index'
   devise_for :users, controllers: { sessions: 'users/sessions' }
   resources :chat_rooms, only: [:new, :create, :show, :index]
-  root 'chat_rooms#index'
+  mount ActionCable.server => '/cable'
 end
